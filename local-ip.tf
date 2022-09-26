a5e8339f41e3dde582ba08d70faa996127109613
nareshreddy <nareshreddyn97@gmail.com>
2022-09-26 14:16:34 +0530

    cicd

--- local-ip.tf
+++
@@ -1,7 +0,0 @@
-data "http" "myip" {
-  url = "http://ipv4.icanhazip.com"
-}
-
-locals {
-    myterraform-ip ="${chomp(data.http.myip.body)}/32"
-}
--- user_data.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-# SPDX-License-Identifier: MIT-0
-
-set -o xtrace
-systemctl stop kubelet
-/etc/eks/bootstrap.sh ${cluster_name} 
-
